5e2721e6e71bdeacf74ba44dcb47ab351e21b279
Narin68 <pachnarin68@gmail.com>
2023-03-28 10:09:20 +0700

    new

---
+++ vue-project/src/components/SiteNavigator.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="sticky shadow-lg p-5 flex align-middle">
+    <div class="container flex align-bottom justify-between">
+      <div class="flex">
+        <div class="mr-5"><i class="fa-solid fa-sun"></i></div>
+        <div class="text-lg">The local weather</div>
+      </div>
+      <div class="flex">
+        <div class="text-lg">
+          <i class="fa-solid fa-circle-info cursor-pointer"></i>
+        </div>
+        <div class="text-lg ml-5 cursor-pointer">
+          <i class="fa-solid fa-add"></i>
+        </div>
+      </div>
+    </div>
+  </div>
+  <div class="container"></div>
+</template>
+
+<script setup>
+</script>
